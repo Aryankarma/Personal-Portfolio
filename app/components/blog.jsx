4ac7966442da95e936943e4d04affc19e1148645
Aryan Karma <110737632+Aryankarma@users.noreply.github.com>
2024-01-24 10:01:56 +0000

    capitalization error solve && dateAndTime dot added

--- app/components/blog.jsx
+++ app/components/blog.jsx
@@ -5,61 +5,61 @@ import { useState } from "react"
 const blogData = [{
     heading: "The story of my weekend project - A complete dumpster ",
     subheading:"A story about how my Christmas was ruined by a series of spam, abuse and DDoS attacks on a small app that I built. And what you can learn from it.",
-    minuteRead: "• 3 min read",
+    minuteRead: "3 min read",
     date:"Jan 6 2023"
 },
 {
     heading: "The story of my weekend project - A complete dumpster ",
     subheading:"A story about how my Christmas was ruined by a series of spam, abuse and DDoS attacks on a small app that I built. And what you can learn from it.",
-    minuteRead: "• 3 min read",
+    minuteRead: "3 min read",
     date:"Jan 6 2023"
 },
 {
     heading: "The story of my weekend project - A complete dumpster ",
     subheading:"A story about how my Christmas was ruined by a series of spam, abuse and DDoS attacks on a small app that I built. And what you can learn from it.",
-    minuteRead: "• 3 min read",
+    minuteRead: "3 min read",
     date:"Jan 6 2023"
 },
 {
     heading: "The story of my weekend project - A complete dumpster ",
     subheading:"A story about how my Christmas was ruined by a series of spam, abuse and DDoS attacks on a small app that I built. And what you can learn from it.",
-    minuteRead: "• 3 min read",
+    minuteRead: "3 min read",
     date:"Jan 6 2023"
 },
 {
     heading: "The story of my weekend project - A complete dumpster ",
     subheading:"A story about how my Christmas was ruined by a series of spam, abuse and DDoS attacks on a small app that I built. And what you can learn from it.",
-    minuteRead: "• 3 min read",
+    minuteRead: "3 min read",
     date:"Jan 6 2023"
 },
 {
     heading: "The story of my weekend project - A complete dumpster ",
     subheading:"A story about how my Christmas was ruined by a series of spam, abuse and DDoS attacks on a small app that I built. And what you can learn from it.",
-    minuteRead: "• 3 min read",
+    minuteRead: "3 min read",
     date:"Jan 6 2023"
 },
 {
     heading: "The story of my weekend project - A complete dumpster fire, and everything I learnt from it.",
     subheading:"A story about how my Christmas was ruined by a series of spam, abuse and DDoS attacks on a small app that I built. And what you can learn from it.",
-    minuteRead: "• 3 min read",
+    minuteRead: "3 min read",
     date:"Jan 26 2023"
 },
 {
     heading: "The story of my weekend project - A complete dumpster fire, and everything I learnt from it.",
     subheading:"A story about how my Christmas was ruined by a series of spam, abuse and DDoS attacks on a small app that I built. And what you can learn from it.",
-    minuteRead: "• 3 min read",
+    minuteRead: "3 min read",
     date:"Jan 26 2023"
 },
 {
     heading: "The story of my weekend project - A complete dumpster fire, and everything I learnt from it.",
     subheading:"A story about how my Christmas was ruined by a series of spam, abuse and DDoS attacks on a small app that I built. And what you can learn from it.",
-    minuteRead: "• 3 min read",
+    minuteRead: "3 min read",
     date:"Jan 26 2023"
 },
 {
     heading: "The story of my weekend project - A complete dumpster fire, and everything I learnt from it.",
     subheading:"A story about how my Christmas was ruined by a series of spam, abuse and DDoS attacks on a small app that I built. And what you can learn from it.",
-    minuteRead: "• 3 min read",
+    minuteRead: "3 min read",
     date:"Jan 26 2023"
 }]
 
@@ -80,8 +80,8 @@ const Blog = ({theme}) => {
                     <p className={styles.subheading}>{input.subheading}</p>
                 </div>
                 <div className={styles.DateAndTimeContainer}>
-                    <p className={`${styles.minuteRead} ${theme == "Dark" ? styles.minuteReadDark : styles.minuteReadLight}`}>{input.minuteRead}</p>
-                    <p className={`${styles.date} ${theme == "Dark" ? styles.dateDark : styles.dateLight}`}>{input.date}</p>
+                    <p className={`${styles.minuteRead} ${theme == "Dark" ? styles.minuteReadDark : styles.minuteReadLight}`}><span className={styles.removeDesktop}>• </span> {input.minuteRead}</p>
+                    <p className={`${styles.date} ${theme == "Dark" ? styles.dateDark : styles.dateLight}`}> {input.date}</p>
                 </div>
             </div>
             <hr className={`${styles.hr} ${theme == "Dark" ? styles.hrDark : styles.hrLight}`}/>
